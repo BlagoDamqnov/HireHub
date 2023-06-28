ceb5e4d7254530866ec952e13c28f39ee7f1bfb1
BlagoDamqnov <chessplayer89@abv.bg>
2023-06-28 14:45:07 +0300

    Apply entity validation for Job create

--- HireHub.Data.Models/Entities/Job.cs
+++ HireHub.Data.Models/Entities/Job.cs
@@ -10,16 +10,23 @@ namespace HireHub.Data.Entities
     using System.Text;
     using System.Threading.Tasks;
 
+    using static Common.EntityValidation.Job;
     public class Job
     {
         [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
         public string CreatorId { get; set; } = null!;
 
-        [ForeignKey(nameof(CreatorId))] 
+        [ForeignKey(nameof(CreatorId))]
         public IdentityUser User { get; set; } = null!;
+
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength,ErrorMessage= "Title must be between {2} and {1} length")]
         public string Title { get; set; } = null!;
+
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength,ErrorMessage = "Description must be between {2} and {1} length")]
         public string Description { get; set; } = null!;
+
+        [StringLength(RequirementsMaxLength, MinimumLength = RequirementsMinLength,ErrorMessage = "Requirements must be between {2} and {1} length")] 
         public string Requirements { get; set; } = null!;
         public int CategoryId { get; set; }
 
@@ -29,9 +36,15 @@ namespace HireHub.Data.Entities
 
         [ForeignKey(nameof(LocationId))]
         public Town Location { get; set; } = null!;
+
+        [StringLength(MinSalaryMaxValue, MinimumLength = MinSalaryMinValue,ErrorMessage = "Min. Salary must be between {2} and {1} range")]
         public decimal MinSalary { get; set; }
+
+        [StringLength(MaxSalaryMaxValue, MinimumLength = MaxSalaryMinValue,ErrorMessage = "Max. Salary must be between {2} and {1} range")]
         public decimal? MaxSalary { get; set; }
         public DateTime CreatedOn { get; set; }
+
+        [StringLength(LogoUrlMaxLength, MinimumLength = LogoUrlMinLength,ErrorMessage = "Logo must be between {2} and {1} range")]
         public string LogoUrl { get; set; } = null!;
         public bool IsDeleted { get; set; } = false;
         public bool IsApproved { get; set; } = false;
--- HireHub.Web.ViewModels/Jobs/CreateJobVM.cs
+++ HireHub.Web.ViewModels/Jobs/CreateJobVM.cs
@@ -1,4 +1,5 @@
-﻿using HireHub.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using HireHub.Data.Entities;
 using HireHub.Web.ViewModels.Categories;
 using HireHub.Web.ViewModels.Countries;
 using HireHub.Web.ViewModels.Towns;
@@ -11,12 +12,19 @@ namespace HireHub.Web.ViewModels.Jobs
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
-
+    using static HireHub.Common.EntityValidation.Job;
     public class CreateJobVM
-    { 
+    {
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength, ErrorMessage = "Title must be between {2} and {1} length")]
         public string Title { get; set; } = null!;
+
+        [StringLength(LogoUrlMaxLength, MinimumLength = LogoUrlMinLength, ErrorMessage = "Logo must be between {2} and {1} range")]
         public string Logo { get; set; } = null!;
+
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength, ErrorMessage = "Description must be between {2} and {1} length")]
         public string Description { get; set; } = null!;
+
+        [StringLength(RequirementsMaxLength, MinimumLength = RequirementsMinLength, ErrorMessage = "Requirements must be between {2} and {1} length")]
         public string Requirements { get; set; } = null!;
         public int CategoryId { get; set; }
         public ICollection<CategoryVM> Categories { get; set; } = new List<CategoryVM>();
@@ -24,10 +32,9 @@ namespace HireHub.Web.ViewModels.Jobs
         public ICollection<CountryVM> Countries { get; set; } = new List<CountryVM>();
         public int TownId { get; set; }
         public ICollection<TownVM> Towns{ get; set; } = new List<TownVM>();
+        
         public decimal MinSalary { get; set; }
+        
         public decimal? MaxSalary { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public bool IsDeleted { get; set; } = false; // For change
-        public bool IsApproved { get; set; } = false; // For change
     }
 }

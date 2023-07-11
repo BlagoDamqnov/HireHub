7964b6082c7ea9bf5f2a93b3ceae62b1b03a7208
Blagovest Damyanov <91145357+BlagoDamqnov@users.noreply.github.com>
2023-07-11 15:42:31 +0300

    Make fields in Company entity to be UNIQUE

---
+++ HireHub.Data/Migrations/20230711115223_AddUniqueConstraintOnCompany.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HireHub.Data.Migrations
+{
+    public partial class AddUniqueConstraintOnCompany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_ContactEmail",
+                table: "Companies",
+                column: "ContactEmail",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_ContactPhone",
+                table: "Companies",
+                column: "ContactPhone",
+                unique: true,
+                filter: "[ContactPhone] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_Name",
+                table: "Companies",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_ContactEmail",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_ContactPhone",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_Name",
+                table: "Companies");
+        }
+    }
+}

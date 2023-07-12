19d0f244f12e91cc04f952f032925efb368006be
Blagovest Damyanov <91145357+BlagoDamqnov@users.noreply.github.com>
2023-07-12 19:37:46 +0300

    Implemented custom error 404 page

--- HireHub/Controllers/HomeController.cs
+++ HireHub/Controllers/HomeController.cs
@@ -19,9 +19,14 @@
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if(statusCode == 404)
+            {
+                return View("404");
+            }
+
+            return View();
         }
     }
 }
--- HireHub/Program.cs
+++ HireHub/Program.cs
@@ -52,7 +52,7 @@ namespace HireHub
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
                 app.UseHsts();
             }
 
